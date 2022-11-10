d54c8cfc01377a4bcf549ff6423a8b72559df048
ByGio1 <home@ettoreandgio.com>
2022-11-10 21:50:28 +0100

    entrar

--- webp.js
+++ webp.js
@@ -1,25 +1,29 @@
-import imagemin from "imagemin";
-import webp from "imagemin-webp";
-
-const outputFolder = "./public/dist/assets";
+const outputFolder = "./images/webp";
 const produceWebP = async () => {
-  await imagemin(["images/*.png"], {
-    destination: outputFolder,
-    plugins: [
-      webp({
-        lossless: true,
-      }),
-    ],
-  });
-  console.log("PNGs processed");
-  await imagemin(["images/*.{jpg,jpeg}"], {
-    destination: outputFolder,
-    plugins: [
-      webp({
-        quality: 65,
-      }),
-    ],
-  });
-  console.log("JPGs and JPEGs processed");
+    // Load ESM modules using import(),
+    // it returns a Promise which resolves to
+    // default export as 'default' and other named exports.
+    // In this case we need default export.
+    const imagemin = (await import("imagemin")).default;
+    const webp = (await import("imagemin-webp")).default;
+    await imagemin(["images/*.png"], {
+        destination: outputFolder,
+        plugins: [
+            webp({
+                lossless: true,
+            }),
+        ],
+    });
+    console.log("PNGs processed");
+    await imagemin(["images/*.{jpg,jpeg}"], {
+        destination: outputFolder,
+        plugins: [
+            webp({
+                quality: 65,
+            }),
+        ],
+    });
+    console.log("JPGs and JPEGs processed");
 };
 produceWebP();
+
